4fdae2c637b66a8cc657f8ea8613f6a50af252a9
mike <mikethemerchant@gmail.com>
2022-11-08 08:35:18 -0500

    my version

--- JavaScriptBook/chessBoard.js
+++ JavaScriptBook/chessBoard.js
@@ -1,1 +1,26 @@
-console.log("what it is");
+let boardSize = 15;
+let row = "";
+
+for (let y = 1; y <= boardSize; y++){
+    for (let x = 1; x <= boardSize; x++){
+        if(y % 2){
+            if(x % 2){
+                row = row + " ";
+            } 
+            else{
+                row = row + "#";
+            }
+        } 
+        else {
+            if(x % 2){
+                row = row + "#";
+            } 
+            else{
+                row = row + " ";
+            }
+        }
+        //console.log(`(${y},${x})`);
+    }
+    console.log(row);
+    row = "";
+}
