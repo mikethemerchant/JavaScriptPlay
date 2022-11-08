eeb39bfa5c7f129a1e2d90e94dd3a6f97b395cc9
mike <mikethemerchant@gmail.com>
2022-11-08 10:58:42 -0500

    from the book

--- JavaScriptBook/fizzBuzz.js
+++ JavaScriptBook/fizzBuzz.js
@@ -1,19 +1,10 @@
 
+// NOTE: The OR operator returns the left side if it's true, if it's false returns the right side.
+
 for (let i = 1; i <= 100; i++)
 {
-    if(i % 3 == 0 || i % 5 == 0) {
-        if(i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
-        }
-        else {
-            if(i % 3 == 0)
-                console.log("Fizz");
-            else
-                console.log("Buzz");
-        }
-
-    }    
-    else {
-        console.log(i);
-    }
+    let result = "";
+    if(i % 3 == 0) result += "Fizz";
+    if(i % 5 == 0) result += "Buzz";
+    console.log(result || i);
 }
