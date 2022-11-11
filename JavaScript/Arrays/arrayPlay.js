65c2ebf58e82eff38ce55c9ec1faa2364dcddc7c
mike <mikethemerchant@gmail.com>
2022-11-11 11:08:25 -0500

    array play

--- JavaScript/Arrays/arrayPlay.js
+++ JavaScript/Arrays/arrayPlay.js
@@ -38,8 +38,25 @@ console.log(cars.join());
 delete cars[2]; // leaves undefined in the hole, better to use pop or shift instead
 console.log(cars.join());
 
-const together = cars.concat(trucks);
+console.log("-- try to combine --");
+
 console.log(cars.join());
+console.log(trucks.join());
+const together = cars.concat(trucks);
+console.log(together.join());
+
+console.log("-- splice and slice --");
+together.splice(2,2,"AddMe","andMe");
+console.log(together);
+// then remove them
+together.splice(2,2);
+console.log(together);
+
+// cut out a section
+const words = together.slice(1,2);
+console.log(words);
+console.log(together);
+
 
 
 
