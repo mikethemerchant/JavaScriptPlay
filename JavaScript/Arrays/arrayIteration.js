2c79f7b91f75f291d42362c7b73e84c514a05541
mike <mikethemerchant@gmail.com>
2022-11-14 15:01:17 -0500

    sync new file

---
+++ JavaScript/Arrays/arrayIteration.js
@@ -0,0 +1,46 @@
+const cars = ["Subaru", "Mazda", "Chevy", "Ford"];
+
+const numbers = [5,1,8,4,5,7];
+
+// filter creates a new function with elements that meet criteria passed in with function
+const numbersGreaterThan4 = numbers.filter((value) => value > 4);
+console.log(numbersGreaterThan4);
+
+// reduce runs a function on each element and puts in total value
+// total is the intial value and previously returned value and can accept an intial start IE:10
+let sum = numbers.reduce( (total,value) => total + value, 10);
+console.log(sum);
+
+
+// every() checks if all array elements are passes a test
+let over2 = numbers.every( (value) => value > 2);
+console.log(over2);
+
+
+// some() checks if some array elements are passes a test
+let someOver2 = numbers.some( (value) => value > 2);
+console.log(someOver2);
+
+
+// index of returns the position of the element [zero based]
+let pos = numbers.indexOf(5);
+console.log(pos);
+
+
+// or the last pos
+let lastPos = numbers.lastIndexOf(5);
+console.log(lastPos);
+
+// find first element that matches criteria  [doesn't work in IE new in ES6 2015]
+let find7 = numbers.find( (value) => value == 7 );
+console.log(find7);
+
+// find INDEX of the first element that matches criteria  [doesn't work in IE new in ES6 2015]
+let find8 = numbers.findIndex( (value) => value == 8 );
+console.log(find8);
+
+// make an array from a anyh object with a lenth property [string] or iterable object.
+const myArr = Array.from("ABCDEFG");
+
+for( let x of myArr)
+    console.log(x);
