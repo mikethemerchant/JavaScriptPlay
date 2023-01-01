6819b542b62d0eb328a4e61ec2a24ff584c4086e
mike <mikethemerchant@gmail.com>
2023-01-01 13:18:57 -0500

    this is a static class method

--- JavaScript/Classes/eldenRing.js
+++ JavaScript/Classes/eldenRing.js
@@ -7,6 +7,9 @@ class knight {
     display  () {
         return `${this.vigor}:${this.strength}:${this.endurance}`;
     }
+    static printNameClass (toonName) {  // add static to make it so you don't have to instanciate to call it.
+        return "I am a knight by the name of " + toonName;
+    }
 }
 
 let lvl50 = new knight(35,31,20);
@@ -14,3 +17,5 @@ let lvl50 = new knight(35,31,20);
 var toPass = JSON.stringify(lvl50);
 
 console.log(toPass);
+
+console.log(knight.printNameClass("Tordack"));
