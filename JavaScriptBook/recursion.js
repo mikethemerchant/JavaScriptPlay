e399958d9d6f40f96fba06a7529d98c1979a94e1
mike <mikethemerchant@gmail.com>
2023-01-02 11:37:54 -0500

    dont understnd this one

--- JavaScriptBook/recursion.js
+++ JavaScriptBook/recursion.js
@@ -1,9 +1,21 @@
 // they want us to use recursion, not sure how todo it that way..
 // create a recursive function that accepts one positive whole number and returns true if number is even and false if number is odd
 
-function isEven (number) {
-    let test = number % 2;
-    return (test == 0) ? true : false;
+function isEven (n) {
+    if(n == 0) {
+        console.log("zero:" + n);
+        return true;
+    } else if (n == 1) {
+        console.log("1:" + n);
+        return false;
+    } else {
+        console.log("else:" + n);
+        isEven(n - 2);
+    }
 }
 
-console.log(isEven(4));
+if(isEven(3) == true) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
