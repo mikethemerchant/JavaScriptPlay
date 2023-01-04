1f180e19649be4d00173faf8b7a59e8baba2512b
mike <mikethemerchant@gmail.com>
2023-01-04 16:36:44 -0500

    this twisted my head but I got it

--- JavaScript/async/asyncSample.js
+++ JavaScript/async/asyncSample.js
@@ -6,30 +6,36 @@ function displayer(thing) {
 async function myFunction () {
     return "Hello";
 }
+// same as:
+function myPromiseFunction() {
+    return Promise.resolve("Hello");
+  }
 
-// same as
-function myPromiseFunction () {
-    return Promise.resolve("PR Hello");
-}
-
+// then after the promise is resolved do this;
 myFunction().then(
-    function(value) {displayer(value);},
-    function(error) {displayer(error);}
-);
-
-myPromiseFunction().then(
-    function(value) {displayer(value);},
-    function(error) {displayer(error);}
+    function(value) {displayer(value);}
 );
 
 // The await keyword can only be used inside an async function.
 // The await keyword makes the function pause the execution and wait for a resolved promise before it continues:
 
 async function myDisplay() {
-    let myPromise = new Promise( function(resolve,reject){
-        resolve("I love you.");
+    let myPromise = new Promise( function(resolve) {
+        setTimeout(()=> {resolve("I love you.");},2000);
     });
-    await myPromise;
+    console.log(await myPromise);
 }
 
 myDisplay();
+
+// chatGPT:
+// async returns a new promise so you don't have to create a new promise
+// put setTimeout directly as the resolve
+
+async function printHiMike() {
+    console.log(await new Promise(resolve => setTimeout(resolve, 4000, "Hi Mike")));
+}
+
+printHiMike();
+
+  
