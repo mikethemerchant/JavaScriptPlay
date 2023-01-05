948328d746d888e780c13f93e39f2d58e167f6c3
mike <mikethemerchant@gmail.com>
2023-01-05 12:15:08 -0500

    func for dom

--- JavaScript/DOM/scripts.js
+++ JavaScript/DOM/scripts.js
@@ -15,3 +15,37 @@ function changeByClass() {
         classList[i].innerHTML = "changed with class";
     }
 }
+
+function disableButton() {
+    document.getElementById("changeTest").setAttribute("disabled","disabled");
+}
+
+function enableButton() {
+    document.getElementById("changeTest").removeAttribute("disabled");
+}
+
+function changeStyle() {
+    const test = document.getElementById("test"); 
+    test.style.color = "red";
+}
+
+function querySelect() {
+    const x = document.querySelectorAll("p.bye");
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.color = "blue";
+    }
+}
+
+function putBelow() {
+    const f = document.forms["inputForm"];
+    const y = document.getElementById("below");
+    let text = "";
+    for (let i = 0; i < f.length ;i++) {
+        text += f.elements[i].value + "<br>";
+      }
+    y.innerHTML = text;
+}
+
+function opps() {
+    document.write("opps.");
+}
