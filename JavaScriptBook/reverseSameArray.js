3263491fb2c9ca6af15357bf2d4a56304e88c8bc
MikeTheMerchant <mikethemerchant@gmail.com>
2023-01-11 08:11:46 -0500

    my version of this solution.

---
+++ JavaScriptBook/reverseSameArray.js
@@ -0,0 +1,28 @@
+// write a function that takes an array as an argument and productes a new array that has the same elements in the inverse order
+// write a function that takes and array as an argment and reverses the order of the elements
+// the reverse method can not be used.
+
+function reverseAry(ary) {
+    let newAry = [];
+
+    for(let i=ary.length;i > 0;i--) {
+        newAry.push(i);
+    }
+
+    let size = ary.length
+    for(let i=0;i <= size;i++) {
+        ary.pop(i);
+    }
+
+    for(let i = 0;i < newAry.length;i++) {
+        ary.push(newAry[i]);
+    }
+
+    
+    return ary;
+}
+
+const ary1 = [1,2,3,4];
+const reversed = reverseAry(ary1);
+console.log(ary1);
+
