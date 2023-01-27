2685993b55c76bf9ce7ae14b1a6b65bf31de279d
Michael Bender <mikethemerchant@gmail.com>
2023-01-27 17:13:56 -0500

    some play html done

---
+++ HTML/friday.js
@@ -0,0 +1,44 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function save() {
+    const input = document.getElementById("input");
+    localStorage.input = input.value;
+
+}
+
+function recall() {
+    document.getElementById("stored").innerHTML  = localStorage.input;
+    localStorage.removeItem(input)
+}
+
+var w;
+
+function startWorker() {
+  if (typeof(Worker) !== "undefined") {
+    if (typeof(w) == "undefined") {
+      w = new Worker("demo_workers.js");
+    }
+    w.onmessage = function(event) {
+      document.getElementById("result").innerHTML = event.data;
+    };
+  } else {
+    document.getElementById("result").innerHTML = "Sorry! No Web Worker support.";
+  }
+}
+
+function stopWorker() {
+  w.terminate();
+  w = undefined;
+}
