2340de42d320111d9c13a0eeb2165ab3843c2a8d
mikethemerchant <mikethemerchant@gmail.com>
2023-06-26 11:21:04 -0400

    theirs

--- JavaScriptBook/everything.js
+++ JavaScriptBook/everything.js
@@ -1,24 +1,36 @@
-//const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//const numbers = [3, 2, 1];
-const numbers = [3, 3, 3];
-
 // predicate function takes an element and returns true or false usually used inside .some or .every
 
 // takes an array and predicate 
 function myEveryFunc( array, predicate) {
-    let allequal = false;
     for(let i=0;i<=array.length-1;i++) {
-        console.log(array[i]);
-        if( predicate(array[i]) == false )
-        {
-            break;
-        }
-        if( i === array.length-1 )
-        {
-            allequal = true;
-        }
+        if(!predicate(array[i]))
+            return false;
     }
-    return allequal;
+    return true;
 }
 
-console.log( myEveryFunc(numbers,(p) => p === 3) );
+console.log(myEveryFunc([1, 3, 5], n => n < 10));
+// → true
+console.log(myEveryFunc([2, 4, 16], n => n < 10));
+// → false
+console.log(myEveryFunc([], n => n < 10));
+// → true
+
+function every2(array, predicate) {
+    return !array.some(element => !predicate(element));
+  }
+
+  console.log(every2([1, 3, 5], n => n < 10));
+  // → true
+  console.log(every2([2, 4, 16], n => n < 10));
+  // → false
+  console.log(every2([], n => n < 10));
+  // → true
+
+  // this makes my brain hurt a little... NOT some = every?
+  numbers = [2,2,2,2];
+
+  function no(thing) {
+    return thing === 2;
+  }
+  console.log('some: ' + !numbers.some(e => !no(e)));
