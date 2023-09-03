f2573657a5e3061fb40bced65e9dde52328090e9
Michael Bender <mikethemerchant@gmail.com>
2023-09-03 09:44:32 -0400

    not done just started

---
+++ JavaScriptBook/groups.js
@@ -0,0 +1,18 @@
+class Group {
+    constructor (item) {
+        const group = [];
+        group.push(item);
+    }
+    add (item) {
+        this.group.push(add);
+    }
+
+    display () {
+        console.log(this.group);
+    }
+}
+
+let g = new Group('mike');
+g.add('ashley');
+
+g.display();
