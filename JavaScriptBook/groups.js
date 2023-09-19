376f0d27fb4647fa042770ac4320c9110cb57f5d
Michael Bender <michael.bender@spartronics.com>
2023-09-19 10:53:34 -0400

    i made my own iteration

--- JavaScriptBook/groups.js
+++ JavaScriptBook/groups.js
@@ -1,20 +1,21 @@
 class Group {
     constructor () {
-        this.group = [];
-    }
-    add (value) {
-        this.group.push(value);
-    }
-    delete(value) {
-        this.group = this.group.filter(v => v != value);
+        this.index = 0;
+        this.list = [];
     }
 
+    add (value) {
+        this.list.push(value);
+    }
+    delete(value) {
+        this.list = this.list.filter(v => v != value);
+    }
     display () {
-        console.log(this.group);
+        console.log(this.list);
     }
 
     has(value) {
-        for(g of this.group) {
+        for(g of this.list) {
             if(g === value) {
                 return true;
             }
@@ -22,19 +23,43 @@ class Group {
         return false;
     }
 
+    [Symbol.iterator]() {
+        return new GroupIterator(this);
+    }
+
+
     // static from(newGroup) {
 
     // }
 }
 
+class GroupIterator {
+    constructor(group) {
+        this.group = group;
+        this.index = 0;
+    }
+
+    next() {
+        
+        if (this.index >= this.group.list.length) {
+            return {done: true};
+        } else {
+            let value = {value: this.group.list[this.index]}
+            this.index++;
+            return value;
+        }
+    }
+}
+
 let g = new Group();
 g.add('mike');
 g.add('bob');
-
-g.display();
-
-g.delete('mike');
+g.add('ashley');
 g.add('roy');
 
 g.display();
-console.log(g.has('ashley'));
+
+for(let v of g) {
+    console.log(v);
+}
+
