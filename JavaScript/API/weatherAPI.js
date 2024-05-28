dad752a187b28f0d2ec493c51a627f1a4e5fc18a
mikethemerchant <mikethemerchant@gmail.com>
2024-05-28 09:11:22 -0400

    test again

--- JavaScript/API/weatherAPI.js
+++ JavaScript/API/weatherAPI.js
@@ -1,6 +1,5 @@
 
 const urlGeoLocation = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={335af534491b02aaffd2ef3fe834c1ae}'
-//const urlGeoLocation = 'http://api.openweathermap.org/geo/1.0/zip?zip={18614},{USA}&appid={335af534491b02aaffd2ef3fe834c1ae}'
 
 const getData = async () => {
     try {
