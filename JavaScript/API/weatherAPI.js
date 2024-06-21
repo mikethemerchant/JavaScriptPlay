5b18e78926685bdb54b7db148f25be58b493fd61
mikethemerchant <mikethemerchant@gmail.com>
2024-06-21 16:10:53 -0400

    not sure if this works

--- JavaScript/API/weatherAPI.js
+++ JavaScript/API/weatherAPI.js
@@ -1,5 +1,5 @@
 
-const urlGeoLocation = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={335af534491b02aaffd2ef3fe834c1ae}'
+const urlGeoLocation = 'https://reqres.in/api/users'
 
 const getData = async () => {
     try {
---
+++ JavaScript/Arrays/arrayTest.js
@@ -0,0 +1,12 @@
+// create array of cars
+const cars = ['Toyota', 'Honda', 'Ford', 'Chevrolet'];
+
+// Using a for loop
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+
+// Using forEach
+cars.forEach(function(car) {
+  console.log(car);
+});
